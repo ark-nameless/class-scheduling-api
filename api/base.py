15faa363f7a1659ca3c2931f976808d7e100ca86
ark-nameless <116332762+ark-nameless@users.noreply.github.com>
2022-11-08 07:47:49 +0800

    added departments route base

--- api/base.py
+++ api/base.py
@@ -5,4 +5,4 @@ from api.v1 import departments
 
 api_router = APIRouter()
 
-api_router.include_router(departments.router, prefix="/students", tags=['Department'])
+api_router.include_router(departments.router, prefix="/departments", tags=['Department'])
